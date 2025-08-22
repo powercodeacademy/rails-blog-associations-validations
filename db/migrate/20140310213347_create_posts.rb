8607f641259fab273b0e622d88c7ca815409d7ca
Mike <mclancy96@gmail.com>
2025-08-22 10:02:01 -0400

    Change migration

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :name
-
+      t.text :context
       t.timestamps null: false
     end
   end
